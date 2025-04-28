27d9d34d42122124fdedba3d39804e79e68e7370
jtovar <jtovar@ks2.tech>
2025-04-28 15:49:35 -0400

    Agregando componentes faltantes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,31 +1,31 @@
 import { useState } from "react"
-import { AddCategory } from "./components/AddCategory";
-
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
-const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+const [categories, setCategories] = useState(['One Punch']);
 
 const onAddCategory = (newCategory) => {
-
+  if ( categories.includes(newCategory) ) return;
   setCategories([newCategory, ...categories]);
 }
 
   return (
-    <>
+    <> 
     
       <h1>GifExpertApp</h1>
       <AddCategory 
-       /*  setCategories={setCategories} */
        onNewCategory={value => onAddCategory(value)}
+       currentCategories={categories}
       />
 
-      <ol>
-        { categories.map(category => (
-            <li key={category}>{category}</li>
+        { categories.map( (category) => (
+            <GifGrid 
+              key={category}  
+              category={category} 
+            />
           )) 
-        }
-      </ol>
+        } 
 
     </>
   )
